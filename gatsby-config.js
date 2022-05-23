c32f46e06d479aa5373598b9739056c383b507c2
Bassel Issa <bassel@y.team>
2022-05-23 18:40:00 +0100

    update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Gatsby Headless Blog using NetlifyCMS`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Y Team`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
     },
   },
   plugins: [
