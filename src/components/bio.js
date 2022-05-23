948ac63dcee5c442ed83c6f70419555740a8c012
Bassel Issa <bassel@y.team>
2022-05-23 20:05:14 +0100

    Fixing Master

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,9 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src=""
+
+        src="https://y.team/wp-content/uploads/2019/07/y-purple.png"
+
         width={50}
         height={50}
         quality={95}
